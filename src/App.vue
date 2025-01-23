94b1b25f8d4449445cb7a750e02adeb71042d726
Maks <max.borovique@gmail.com>
2025-01-23 14:56:14 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <h1>Hello, Max</h1>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ src/components/ui/form.vue
@@ -0,0 +1,13 @@
+<template >
+<form action="submit">
+  <input type="text">
+</form>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+
+</style>
