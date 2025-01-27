8c618366722ff7cfbc94d1cf9f98e6b50879f6e4
Mr.Borovique <max.borovique@gmail.com>
2025-01-27 15:47:09 +0200

    change item heigth

--- vars.ts
+++ vars.ts
@@ -1,4 +1,4 @@
 export const BASE_URL: string = 'https://dummyjson.com';
 export const LIMIT: number = 100;
-export const ITEM_HEIGHT = 210;
+export const ITEM_HEIGHT = 220;
 export const CONTAINER_HEIGHT = 800;
